047cc808037da04f6270b2ad2058aede0b438be1
Alexander Weigl <spamling@web.de>
2014-10-11 05:12:05 +0200

    added prelude modules

---
+++ personal/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-math-abbrev-prefix "æ")
+ '(TeX-PDF-mode t t)
+ '(TeX-auto-save t t)
+ '(TeX-fold-mode 1)
+ '(TeX-parse-self t t)
+ '(TeX-save-query nil)
+ '(enable-local-variables :all)
+ '(reftex-plug-into-AUCTeX t)
+ '(reftex-save-sparse-info nil)
+ '(yas-global-mode t nil (yasnippet))
+ '(yas-snippet-dirs (quote ("~/.emacs.d/vendor/snippets/" "/home/weigl/.emacs.d/elpa/yasnippet-20140314.255/snippets" "/home/weigl/.emacs.d/elpa/django-snippets-20131229.811/snippets")) nil (yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-empty ((t (:foreground "firebrick"))))
+ '(whitespace-trailing ((t nil))))
