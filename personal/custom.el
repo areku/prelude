814ea5094128488909f542dfaec245b8bca6a393
Alexander Weigl <spamling@web.de>
2014-10-28 10:09:45 +0100

    update custom

--- personal/custom.el
+++ personal/custom.el
@@ -4,14 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-math-abbrev-prefix "æ")
- '(TeX-PDF-mode t t)
- '(TeX-auto-save t t)
+ '(TeX-PDF-mode t)
+ '(TeX-auto-save t)
  '(TeX-fold-mode 1)
- '(TeX-parse-self t t)
+ '(TeX-parse-self t)
  '(TeX-save-query nil)
  '(enable-local-variables :all)
+ '(ispell-dictionary "english")
+ '(next-line-add-newlines t)
  '(reftex-plug-into-AUCTeX t)
  '(reftex-save-sparse-info nil)
+ '(tab-width 4)
  '(yas-global-mode t nil (yasnippet))
  '(yas-snippet-dirs (quote ("~/.emacs.d/vendor/snippets/" "/home/weigl/.emacs.d/elpa/yasnippet-20140314.255/snippets" "/home/weigl/.emacs.d/elpa/django-snippets-20131229.811/snippets")) nil (yasnippet)))
 (custom-set-faces
