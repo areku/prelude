515f51b829808a39aefd4308b50cdf6877a6852b
Alexander Weigl <spamling@web.de>
2014-12-24 05:52:40 +0100

    add ace-jump commands

---
+++ personal/keys.el
@@ -0,0 +1,12 @@
+;;jump with C-c SPC to a word
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+(setq ace-jump-mode-case-fold t)
+
+(setq ace-jump-mode-scope 'global)
+(setq ace-jump-mode-enable-mark-sync t)
